1716882545f5f44e708364a710c187381bec2309
morechiba <morechiba@gmail.com>
2024-09-08 23:55:54 +0300

    релизация клика на кнопки с эффектом: 1) анонимный класс 2 и 3) лямбда-выражения

--- app/src/main/java/com/tagomago/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/tagomago/playlistmaker/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.tagomago.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val buttonSearch = findViewById<Button>(R.id.search)
+
+        val imageClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на кнопку SEARCH", Toast.LENGTH_SHORT).show()
+            }
         }
+
+        buttonSearch.setOnClickListener(imageClickListener)
+
+        val buttonMedia = findViewById<Button>(R.id.media)
+
+        buttonMedia.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку MEDIA LIBRARY", Toast.LENGTH_SHORT).show()
+        }
+
+        val buttonSetting = findViewById<Button>(R.id.setting)
+
+        buttonSetting.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку SETTINGS", Toast.LENGTH_SHORT).show()
+        }
+
     }
+
+
 }
