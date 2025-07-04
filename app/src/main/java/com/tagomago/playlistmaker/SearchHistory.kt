970d7297e8a54b90815c4c818611c6e42372d3b8
morechiba <morechiba@gmail.com>
2025-07-04 21:46:27 +0300

    1) переменная с маленькой буквы 2) isNullOrEmpty(), чтобы избежать использования оператора !! 3) Значение для gson.fromJson вынесено в константу для повышения читабельности кода

--- app/src/main/java/com/tagomago/playlistmaker/SearchHistory.kt
+++ app/src/main/java/com/tagomago/playlistmaker/SearchHistory.kt
@@ -15,7 +15,7 @@ class SearchHistory(val sharedPrefs: SharedPreferences) {
     fun getTracks(): MutableList<Track>{
         val searchHistory: String? = sharedPrefs.getString(SEARCH_HISTORY, "")
         var trackList: MutableList<Track> = mutableListOf<Track>()
-        if(searchHistory?.isEmpty()!!){
+        if(searchHistory?.isNullOrEmpty()){
 
         } else {
             val item = object : TypeToken<MutableList<Track>>() {}.type
@@ -26,8 +26,8 @@ class SearchHistory(val sharedPrefs: SharedPreferences) {
 
     fun saveTrack(track: Track) {
         var searchHistory: String? = sharedPrefs.getString(SEARCH_HISTORY, "")
-
-        var trackList: MutableList<Track> = gson.fromJson(searchHistory, object : TypeToken<MutableList<Track>>() {}.type) ?: mutableListOf<Track>()
+        val item = object : TypeToken<MutableList<Track>>() {}.type
+        var trackList: MutableList<Track> = gson.fromJson(searchHistory, item) ?: mutableListOf<Track>()
         trackList.removeIf { it == track }
         if (trackList.size == 10) {
             trackList.removeAt(0)
